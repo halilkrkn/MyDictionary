b8a765965b3df3c625be7a4f26e214c099c12889
halilkrkn <Halilkrkn000@gmail.com>
2023-09-17 20:39:19 +0300

    Retrofit and Room Database Setup

---
+++ app/src/main/java/com/halilkrkn/mydictionary/data/remote/dto/DefinitionDto.kt
@@ -0,0 +1,15 @@
+package com.halilkrkn.mydictionary.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DefinitionDto(
+    @SerializedName("antonyms")
+    val antonyms: List<String>,
+    @SerializedName("definition")
+    val definition: String,
+    @SerializedName("example")
+    val example: String?,
+    @SerializedName("synonyms")
+    val synonyms: List<String>
+)
---
+++ app/src/main/java/com/halilkrkn/mydictionary/data/remote/dto/PhoneticDto.kt
@@ -0,0 +1,15 @@
+package com.halilkrkn.mydictionary.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PhoneticDto(
+    @SerializedName("audio")
+    val audio: String,
+    @SerializedName("license")
+    val license: LicenseDto,
+    @SerializedName("sourceUrl")
+    val sourceUrl: String,
+    @SerializedName("text")
+    val text: String
+)
